3c88c1fd0089dacaf49eea438288edb0331d500d
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-08-09 21:24:49 +1200

    Add importer for simple space delimited xyz point cloud data.

--- tests/test_xyz.py
+++ tests/test_xyz.py
@@ -1,22 +1,22 @@
 import os.path
 import unittest
 
-from cmlibs.importer import pointcloud
-from cmlibs.importer.pointcloud import import_data
-from cmlibs.importer.errors import ImporterImportInvalidInputs, ImporterImportPointCloudError
+from cmlibs.importer import xyz
+from cmlibs.importer.xyz import import_data
+from cmlibs.importer.errors import ImporterImportInvalidInputs, ImporterImportXYZError
 
 from shared import resource_path
 
 
-class PointCloud(unittest.TestCase):
+class XYZ(unittest.TestCase):
 
     def test_parameters(self):
-        parameters = pointcloud.parameters()
+        parameters = xyz.parameters()
         keys = set(parameters.keys())
         exp_keys = {"version", "id", "input", "output", "title", "description"}
 
         self.assertEqual(exp_keys, keys)
-        self.assertEqual("PointCloud", parameters["id"])
+        self.assertEqual("XYZ", parameters["id"])
         self.assertIn("mimetype", parameters["input"])
         self.assertIn("mimetype", parameters["output"])
 
@@ -27,7 +27,7 @@ class PointCloud(unittest.TestCase):
         self.assertRaises(ImporterImportInvalidInputs, import_data, nonexistent_file, output_dir)
 
     def test_import_data_1(self):
-        point_cloud_file = resource_path("point_cloud_1.csv")
+        point_cloud_file = resource_path("small_point_cloud_with_labels.txt")
         output_dir = resource_path("")
 
         output_exf = import_data(point_cloud_file, output_dir)
@@ -36,11 +36,11 @@ class PointCloud(unittest.TestCase):
         with open(output_exf) as f:
             lines = f.readlines()
 
-        self.assertEqual(35, len(lines))
+        self.assertEqual(63, len(lines))
         os.remove(output_exf)
 
     def test_import_data_2(self):
-        point_cloud_file = resource_path("point_cloud_2.csv")
+        point_cloud_file = resource_path("xyz_2.txt")
         output_dir = resource_path("")
 
         output_exf = import_data(point_cloud_file, output_dir)
@@ -49,29 +49,23 @@ class PointCloud(unittest.TestCase):
         with open(output_exf) as f:
             lines = f.readlines()
 
-        self.assertEqual(27, len(lines))
+        self.assertEqual(55, len(lines))
         os.remove(output_exf)
 
     def test_import_data_3(self):
-        point_cloud_file = resource_path("point_cloud_3.csv")
+        point_cloud_file = resource_path("xyz_1.txt")
         output_dir = resource_path("")
 
-        self.assertRaises(ImporterImportPointCloudError, import_data, point_cloud_file, output_dir)
-
-    def test_import_data_4(self):
-        point_cloud_file = resource_path("point_cloud_4.csv")
-        output_dir = resource_path("")
-
-        self.assertRaises(ImporterImportPointCloudError, import_data, point_cloud_file, output_dir)
+        self.assertRaises(ImporterImportXYZError, import_data, point_cloud_file, output_dir)
 
     def test_import_data_invalid_file_1(self):
         nonexistent_file = resource_path("white_image.jpeg")
         output_dir = resource_path("")
 
-        self.assertRaises(ImporterImportPointCloudError, import_data, nonexistent_file, output_dir)
+        self.assertRaises(ImporterImportXYZError, import_data, nonexistent_file, output_dir)
 
     def test_import_data_invalid_file_2(self):
         nonexistent_file = resource_path("plain_text.txt")
         output_dir = resource_path("")
 
-        self.assertRaises(ImporterImportPointCloudError, import_data, nonexistent_file, output_dir)
+        self.assertRaises(ImporterImportXYZError, import_data, nonexistent_file, output_dir)
